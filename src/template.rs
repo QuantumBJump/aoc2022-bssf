935869c637e7e8fe3254389620c4b27df16eedca
Quinn <quantumjump451@gmail.com>
2024-02-22 11:48:30 +0000

    update template to include timestamps & parts

--- src/template.rs
+++ src/template.rs
@@ -1,8 +1,31 @@
-use std::io;
 use crate::utils;
+use std::{time::Instant};
 
-pub fn execute(input_type: &str) -> Result<(), io::Error> {
+pub fn execute(input_type: &str) -> Result<(), Box<dyn error::Error>> {
     let filename = utils::get_input_path(input_type, xx);
     let lines = utils::read_lines(&filename)?;
+    part_1(lines.clone())?;
+    part_2(lines)?
+}
+
+fn part_1(lines: Vec<String>) -> Result<(), Box<dyn error::Error>> {
+    let start = Instant::now();
+    println!("part 1:");
+    let result = 0;
+
+    println!("\tresult: {}", result);
+    let duration = start.elapsed();
+    println("\ttime elapsed: {:?}", duration);
+    Ok(())
+}
+
+fn part_2(lines: Vec<String>) -> Result<(), Box<dyn error::Error>> {
+    let start = Instant::now();
+    println!("part 2:");
+    let result = 0;
+
+    println!("\tresult: {}", result);
+    let duration = start.elapsed();
+    println("\ttime elapsed: {:?}", duration);
     Ok(())
 }

da94794dd4fd3950367f6e493fa427b39efeca76
Quinn <quantumjump451@gmail.com>
2024-02-22 13:56:40 +0000

    fix typo in template

--- src/template.rs
+++ src/template.rs
@@ -4,11 +4,11 @@ use std::{time::Instant};
 pub fn execute(input_type: &str) -> Result<(), Box<dyn error::Error>> {
     let filename = utils::get_input_path(input_type, xx);
     let lines = utils::read_lines(&filename)?;
-    part_1(lines.clone())?;
-    part_2(lines)?
+    part1(lines.clone())?;
+    part2(lines)?
 }
 
-fn part_1(lines: Vec<String>) -> Result<(), Box<dyn error::Error>> {
+fn part1(lines: Vec<String>) -> Result<(), Box<dyn error::Error>> {
     let start = Instant::now();
     println!("part 1:");
     let result = 0;
@@ -19,13 +19,13 @@ fn part_1(lines: Vec<String>) -> Result<(), Box<dyn error::Error>> {
     Ok(())
 }
 
-fn part_2(lines: Vec<String>) -> Result<(), Box<dyn error::Error>> {
+fn part2(lines: Vec<String>) -> Result<(), Box<dyn error::Error>> {
     let start = Instant::now();
     println!("part 2:");
     let result = 0;
 
     println!("\tresult: {}", result);
     let duration = start.elapsed();
-    println("\ttime elapsed: {:?}", duration);
+    println!("\ttime elapsed: {:?}", duration);
     Ok(())
 }
